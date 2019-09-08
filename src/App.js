2727fe0403fc7c500971af8597c6837075f7bcdb
tmm <blocklocmedia@gmail.com>
2019-09-08 08:27:17 -0400

    empty search error validation, alert component. clear search button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import NavBar from 'components/layout/NavBar';
 import Users from 'components/users/Users';
 import Search from 'components/users/Search';
+import Alert from 'components/layout/Alert';
 import axios from 'axios';
 class App extends Component {
 	state = {
 		users: [],
-		loading: false
+		loading: false,
+		alert: null
 	};
 
 	// async componentDidMount() {
@@ -26,13 +28,28 @@ class App extends Component {
 		this.setState({ users: res.data.items, loading: false });
 	};
 
+	clearUsers = () => {
+		this.setState({ users: [], loading: false });
+	};
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg, type } });
+		setTimeout(() => this.setState({ alert: null }), 5000);
+	};
+
 	render() {
+		const { users, loading } = this.state;
 		return (
 			<div className="App">
 				<NavBar />
 				<div className="container">
-					<Search searchUsers={this.searchUsers} />
-					<Users loading={this.state.loading} users={this.state.users} />
+					<Alert alert={this.state.alert} />
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={users.length > 0 ? true : false}
+						setAlert={this.setAlert}
+					/>
+					<Users loading={loading} users={users} />
 				</div>
 			</div>
 		);
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -5,15 +5,24 @@ export default class Search extends Component {
 		text: ''
 	};
 	static propTypes = {
-		searchUser: PropTypes.func.isRequired
+		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.func.isRequired,
+		setAlert: PropTypes.func.isRequired
 	};
 	onSubmit = (e) => {
 		e.preventDefault();
-		this.props.searchUsers(this.state.text);
-		this.setState({ text: '' });
+		if (this.state.text === '') {
+			this.props.setAlert('Please enter something', 'light');
+		} else {
+			this.props.searchUsers(this.state.text);
+			this.setState({ text: '' });
+		}
 	};
 	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
 	render() {
+		const { showClear, clearUsers } = this.props;
 		return (
 			<div>
 				<form onSubmit={this.onSubmit} className="form">
@@ -26,6 +35,12 @@ export default class Search extends Component {
 					/>
 					<input type="submit" value="Search" className="btn btn-dark btn-block" />
 				</form>
+
+				{showClear && (
+					<button className="btn btn-dark btn-block" onClick={clearUsers}>
+						Clear
+					</button>
+				)}
 			</div>
 		);
 	}
